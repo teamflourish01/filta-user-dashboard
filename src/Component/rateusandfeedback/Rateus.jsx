3087262c062c1837d5b766d1bf2d47077d72d1d7
dhruvisonani123 <dhruvisonaniid@gmail.com>
2024-12-23 10:13:06 +0530

    commit rate-us subscribe

--- src/Component/rateusandfeedback/Rateus.jsx
+++ src/Component/rateusandfeedback/Rateus.jsx
@@ -3,30 +3,51 @@ import "../rateusandfeedback/rateus.css"
 export const Rateus = () => {
   return (
     <>
-        <div className="feedback-container">
+    <div className="rateus-overflow">
+    <div className="rateus-container">
+        
+    <div className="rate-us-feedback-main">
+
       <div className="rate-us">
         <h2>Rate Us</h2>
+        <div className="req-bottom-border"></div>
+
+      <div className="rate-innercontainer">
         <div className="google-logo">
           <img src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" alt="Google" />
         </div>
         <div className="rating">
           <span>⭐⭐⭐⭐⭐</span>
-          <p>5/5 Rating From Over 50 Reviews</p>
+          <p className="rate-us-para">5/5 Rating From Over 50 Reviews</p>
         </div>
+        </div>
+        <div className="req-bottom-border"></div>
+
         <a href="#" className="view-reviews">
           View All Reviews
         </a>
       </div>
       <div className="feedback-form">
         <h2>Feedback</h2>
+        <div className="req-bottom-border"></div>
+
         <form>
-          <input type="text" placeholder="Title" className="feedback-input" />
-          <textarea placeholder="Describe Your Request" className="feedback-textarea"></textarea>
-          <button type="submit" className="submit-button">
+        <div className="feed-back-form">
+        <label htmlFor="" className="rate-us-label">Title</label>
+          <input type="text"  className="feedback-input" />
+          <label htmlFor="" className="rate-us-label">Describe Your Request</label>
+          <textarea  className="feedback-textarea"></textarea>
+          </div>
+        <div className="req-bottom-border"></div>
+
+          <button type="submit" className="submit-button-rateus">
             Submit
           </button>
         </form>
       </div>
+    </div>
+   
+    </div>
     </div>
     </>
   )
--- src/Component/support/Support.jsx
+++ src/Component/support/Support.jsx
@@ -1,36 +1,61 @@
-import React from 'react'
-import "../support/support.css"
+import React from "react";
+import "../support/support.css";
+import threads from "../../images/threads.svg";
+import contact from "../../images/conphone.svg";
+import emailicon from "../../images/emailicon.svg";
+import location from "../../images/locationicon.svg";
+import instagram from "../../images/instablack.svg";
+import instagramcolor from "../../images/instagram.svg"
+import twitter from "../../images/twitter.svg";
+import facebook from "../../images/facebook.svg"
+import linkdin from "../../images/linkdin.svg";
+import thread from "../../images/threads.svg"
+
 const Support = () => {
   return (
     <>
-        <div className="contact-container">
-      <h2 className="contact-title">Contact Information</h2>
-      <div className="contact-details">
-        <div className="contact-item">
-          <i className="fas fa-phone"></i>
-          <span>+91 12345 67890</span>
+      <div className="support-overflow">
+      <div className="support-outer-box">
+        <div className="support-align">
+          <div className="contact-container">
+            <div className="support-inner-width">
+              <h2 className="contact-title">Contact Information</h2>
+              <div className="contact-details">
+                <div className="contact-item">
+                <div className="support-contact"></div>
+                 
+                  <span>+91 12345 67890</span>
+                </div>
+                <div className="contact-item support-mail-p">
+                <div className="support-mail"></div>
+                  {/* <img src={emailicon}></img> */}
+                  <span>filta123@gmail.com</span>
+                </div>
+                <div className="contact-item">
+                <div className="support-location"></div>
+                  {/* <img src={location}></img> */}
+                  <span>Gujarat, India</span>
+                </div>
+              </div>
+              <hr className="dividersupport" />
+              <h3 className="follow-title">Follow Us</h3>
+          <div className="social-icons">
+          <div className="icon instagram-icon"></div>
+          <div className=" icon facebook-icon"></div>
+            <div className="icon linkdin-icon"></div>
+            
+            <img src={thread} alt="" />
+            <img src={twitter}></img>
+        
+            <i className="fas fa-times"></i>
+          </div>
+            </div>
+          </div>
         </div>
-        <div className="contact-item">
-          <i className="fas fa-envelope"></i>
-          <span>filta123@gmail.com</span>
-        </div>
-        <div className="contact-item">
-          <i className="fas fa-map-marker-alt"></i>
-          <span>Gujarat, India</span>
         </div>
       </div>
-      <hr className="divider" />
-      <h3 className="follow-title">Follow Us</h3>
-      <div className="social-icons">
-        <i className="fab fa-instagram"></i>
-        <i className="fab fa-facebook"></i>
-        <i className="fab fa-linkedin"></i>
-        <i className="fab fa-google"></i>
-        <i className="fas fa-times"></i>
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Support
+export default Support;
