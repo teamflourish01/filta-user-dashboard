db1c121a075279d7ec7ba62a649280ff088cec8f
Prince Mendpara <prince.m.7405@gmail.com>
2025-01-16 10:17:17 +0530

    payment on subscription

--- src/Component/subscription/Subscription.jsx
+++ src/Component/subscription/Subscription.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "../subscription/subscription.css";
+import { handlePayment } from "../../uttils/Payment";
+import userContext from "../../context/userDetails";
 const Subscription = () => {
   const [activePlan, setActivePlan] = useState("Monthly");
   const [price, setprice] = useState(119);
+  const { userData, AuthorizationToken, getUserData } = useContext(userContext);
+  const uri = process.env.REACT_APP_DEV_URL;
 
   const handleplanschange = (plan) => {
     setActivePlan(plan);
@@ -13,111 +17,162 @@ const Subscription = () => {
     }
   };
 
+  const filterMobile = async () => {
+    let arr = userData?.socialLinks?.filter((e) => e?.platform == "Call");
+    
+    return arr[0]?.url;
+  };
+  const generateExpiry=()=>{
+    if(activePlan=="Monthly"){
+      const today = new Date();
+      // Add one month to the current date
+      const nextMonth = new Date(today.setMonth(today.getMonth() + 1));
+      return nextMonth.toISOString().split("T")[0];
+    }else{
+      const today = new Date();
+      const futureDate = new Date(today.setFullYear(today.getFullYear() + 1));
+      return futureDate.toISOString().split("T")[0]; 
+    }
+  }
+  const handleSuccess=async()=>{
+  try {
+    let data=await fetch(`${uri}/user/edituser`,{
+      body:JSON.stringify({"planExpired":generateExpiry()}),
+      headers:{
+        "Content-Type": "application/json",
+        Authorization: AuthorizationToken,
+      }
+    })
+    data=await data.json()
+    console.log(data,"expiry");
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+  }
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return (
     <>
-
       <div className="overflow-subscription">
-      <div className="pricing-container">
-        <div className="toggle-buttons">
-          <button
-            className={`toggle-button ${
-              activePlan === "Monthly" ? "active" : ""
-            }`}
-            onClick={() => handleplanschange("Monthly")}
-          >
-            Monthly
-          </button>
-          <button
-            className={`toggle-button ${
-              activePlan === "Yearly" ? "active" : ""
-            }`}
-            onClick={() => handleplanschange("Yearly")}
-          >
-            Yearly
-          </button>
-        </div>
-        <div className="pricing-plans">
-          <div className="plan-card">
-            <div className="subscription-padding-top">
-              <h3 className="plan-title">PREMIUM</h3>
-              <div className="subscription-plan-padding">
-              <p className="plan-price">
-              
-                ₹ {price}/-{" "}
-                <span>
-                  ({activePlan === "Monthly" ? "Per Month" : "Per Year"})
-                </span>
-              </p>
+        <div className="pricing-container">
+          <div className="toggle-buttons">
+            <button
+              className={`toggle-button ${
+                activePlan === "Monthly" ? "active" : ""
+              }`}
+              onClick={() => handleplanschange("Monthly")}
+            >
+              Monthly
+            </button>
+            <button
+              className={`toggle-button ${
+                activePlan === "Yearly" ? "active" : ""
+              }`}
+              onClick={() => handleplanschange("Yearly")}
+            >
+              Yearly
+            </button>
+          </div>
+          <div className="pricing-plans">
+            <div className="plan-card">
+              <div className="subscription-padding-top">
+                <h3 className="plan-title">PREMIUM</h3>
+                <div className="subscription-plan-padding">
+                  <p className="plan-price">
+                    ₹ {price}/-{" "}
+                    <span>
+                      ({activePlan === "Monthly" ? "Per Month" : "Per Year"})
+                    </span>
+                  </p>
+                </div>
+                <button
+                  className="plan-button"
+                  onClick={() =>
+                    handlePayment({
+                      price:price*100,
+                      name: userData?.card?.name,
+                      email: userData?.email,
+                      mobile: filterMobile(),
+                      onSuccess: handleSuccess,
+                      userId: userData?._id,
+                    })
+                  }
+                >
+                  Get Started
+                </button>
               </div>
-              <button className="plan-button">Get Started</button>
+              <div className="subscription-border-bottom"></div>
+              <ul className="plan-features">
+                <li>Everything included in Free, plus:</li>
+                <div className="subscription-border-bottom"></div>
+
+                <li>Customer Support</li>
+                <div className="subscription-border-bottom"></div>
+
+                <li>Hide Filta Branding</li>
+                <div className="subscription-border-bottom"></div>
+
+                <li>Contact Form</li>
+                <div className="subscription-border-bottom"></div>
+
+                <li>Multiple Sharing Option</li>
+                <div className="subscription-border-bottom"></div>
+
+                <li>Custom Color</li>
+                <div className="subscription-border-bottom"></div>
+
+                <li>Custom Font</li>
+                <div className="subscription-border-bottom"></div>
+
+                <li>Voice Message</li>
+                <div className="subscription-border-bottom"></div>
+
+                <li>Multi Language Interface</li>
+                <div className="subscription-border-bottom"></div>
+
+                <li>Call to Action Button</li>
+              </ul>
             </div>
-            <div className="subscription-border-bottom"></div>
-            <ul className="plan-features">
-              <li>Everything included in Free, plus:</li>
+            <div className="plan-card">
+              <div className="subscription-padding-top">
+                <h3 className=" team-plan">TEAM</h3>
+                <button className="plan-button contact-button">
+                  Contact Sales
+                </button>
+              </div>
               <div className="subscription-border-bottom"></div>
 
-              <li>Customer Support</li>
-              <div className="subscription-border-bottom"></div>
+              <ul className="plan-features">
+                <li>Everything included in Premium, plus:</li>
+                <div className="subscription-border-bottom"></div>
 
-              <li>Hide Filta Branding</li>
-              <div className="subscription-border-bottom"></div>
+                <li>Costume Dashboard Design</li>
+                <div className="subscription-border-bottom"></div>
 
-              <li>Contact Form</li>
-              <div className="subscription-border-bottom"></div>
+                <li>Costume Company Branding</li>
+                <div className="subscription-border-bottom"></div>
 
-              <li>Multiple Sharing Option</li>
-              <div className="subscription-border-bottom"></div>
+                <li>Unlimited Lead Storage</li>
+                <div className="subscription-border-bottom"></div>
 
-              <li>Custom Color</li>
-              <div className="subscription-border-bottom"></div>
+                <li>Team and Sub-team Manager</li>
+                <div className="subscription-border-bottom"></div>
 
-              <li>Custom Font</li>
-              <div className="subscription-border-bottom"></div>
+                <li>Multiple Template Theme</li>
+                <div className="subscription-border-bottom"></div>
 
-              <li>Voice Message</li>
-              <div className="subscription-border-bottom"></div>
+                <li>Export Data</li>
+                <div className="subscription-border-bottom"></div>
 
-              <li>Multi Language Interface</li>
-              <div className="subscription-border-bottom"></div>
-
-              <li>Call to Action Button</li>
-            </ul>
-          </div>
-          <div className="plan-card">
-            <div className="subscription-padding-top">
-              <h3 className=" team-plan">TEAM</h3>
-              <button className="plan-button contact-button">
-                Contact Sales
-              </button>
+                <li>Business Level Support</li>
+              </ul>
             </div>
-            <div className="subscription-border-bottom"></div>
-
-            <ul className="plan-features">
-              <li>Everything included in Premium, plus:</li>
-              <div className="subscription-border-bottom"></div>
-
-              <li>Costume Dashboard Design</li>
-              <div className="subscription-border-bottom"></div>
-
-              <li>Costume Company Branding</li>
-              <div className="subscription-border-bottom"></div>
-
-              <li>Unlimited Lead Storage</li>
-              <div className="subscription-border-bottom"></div>
-
-              <li>Team and Sub-team Manager</li>
-              <div className="subscription-border-bottom"></div>
-
-              <li>Multiple Template Theme</li>
-              <div className="subscription-border-bottom"></div>
-
-              <li>Export Data</li>
-              <div className="subscription-border-bottom"></div>
-
-              <li>Business Level Support</li>
-            </ul>
           </div>
-        </div>
         </div>
       </div>
     </>
